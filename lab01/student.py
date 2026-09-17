d352dcf24e32cdf2473b4dabc6968d3fcd627f38
Magdalena-ace <C00320409@setu.ie>
2026-09-17 18:08:06 +0000

    commit#

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+student_no=input("What is your student number? ")
+year=input("What year are you in? ") 
+
+print("Name: " + name )
+print("Student Number " +student_no)
+print("Year: " +year )
